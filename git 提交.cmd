922f42cf82192d9f1db9c7f4b911d2d9e8573ec1
luohaojun <1417015340@qq.com>
2025-12-29 10:25:54 +0800

    auto commit this version code

--- git 提交.cmd
+++ git 提交.cmd
@@ -4,5 +4,5 @@ git commit -m "auto commit this version code"
 git push origin main 
 
 git remote rm github
-git remote add github git@github.com:neko233-com/db233-go.git
+git remote add github https://github.com/neko233-com/db233-go.git
 git push github main
